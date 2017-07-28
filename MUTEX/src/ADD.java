e2196bafd93ec5aa095ea4b3daf1e0351611cc8c
rushikeshpatil855 <rushikeshpatil855@gmail.com>
2017-07-28 22:05:57 +0530

    Add program

--- MUTEX/src/ADD.java
+++ MUTEX/src/ADD.java
@@ -4,12 +4,14 @@ public class ADD {
     {
         return value1+value2;
     }
-    public static void main(String[] args)
+    public static void main(String args[])
     {
         System.out.println("Enter num1 and num2");
-        int value1 = Integer.parseInt(args[0]);
-        int value2 = Integer.parseInt(args[1]);
+        int value1 = Integer.parseInt(args[1]);
+        int value2 = Integer.parseInt(args[0]);
         int value3 = add1(value1,value2);
         System.out.println(value3);
+        System.out.println("1 : "+value1);
+        System.out.println("2 : "+value2);
     }
 }
