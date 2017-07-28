4be0677c1b5bfa5261fc0971beaa0101aecd4514
rushikeshpatil855 <rushikeshpatil855@gmail.com>
2017-07-28 21:06:06 +0530

    My first commit

---
+++ MUTEX/src/Addition.java
@@ -0,0 +1,29 @@
+/*
+public class Addition {
+	double sum(double numOne,double numTwo)
+	{
+		return numOne + numTwo;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Addition add=new Addition();
+		double result=add.sum(9.11, 11.5);
+		System.out.println("Floating addition is : "+result);
+	}
+
+}*/
+
+
+public class Addition {
+	float sum(float numOne,float numTwo)
+	{
+		return numOne + numTwo;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Addition add=new Addition();
+		float result=add.sum(9.11f, 11.5f);
+		System.out.println("Floating addition is : "+result);
+	}
+
+}
---
+++ MUTEX/src/factorial.java
@@ -0,0 +1,16 @@
+
+public class factorial {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value1 = Integer.parseInt(args[0]);
+		int value2 = Integer.parseInt(args[1]);
+		System.out.println("Value1"+value1);
+		System.out.println("Value2"+value2);
+		
+	}
+
+}
