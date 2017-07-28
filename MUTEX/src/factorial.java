b472f1e9379c5cb17e437d1ec3d6d30eab24f066
rushikeshpatil855 <rushikeshpatil855@gmail.com>
2017-07-28 22:12:10 +0530

    Hello

--- MUTEX/src/factorial.java
+++ MUTEX/src/factorial.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class factorial {
 
@@ -6,8 +7,16 @@ public class factorial {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		
+		int i=1,number;
+		int fact=1;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number ");
+		number=sc.nextInt();
+		for(i=1;i<=number;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println(fact);
 	}
 
 }
